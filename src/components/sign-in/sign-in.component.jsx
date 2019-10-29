9d70381797a41386919cf1e7fffb3e9544fc945f
draddo11 <38097150+draddo11@users.noreply.github.com>
2019-10-29 03:49:56 +0000

    implemeted ign in with email

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FormInput from '../form-inputs/form-input.component';
 import CustomButton from '../custom-buttons/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth,signInWithGoogle} from '../../firebase/firebase.utils'
 
 import  './sign-in.styles.scss';
 
@@ -16,11 +16,19 @@ class SignIn extends  React.Component{
           password:''
       }
   } 
-  handleSubmit= event =>{
+  
+  handleSubmit= async event =>{
     event.preventDefault();
 
-    this.setState({email:'',password:''})
-}
+    const {email, password}= this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email:'', password:''})
+    }catch(error){
+     console.log(error)
+    }
+    
+};
 
 handleChange = event =>{
     const {value, name} = event.target;
