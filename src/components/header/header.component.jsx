d7e60fc0f689d06326309f42c3307894ae22fef7
draddo11 <38097150+draddo11@users.noreply.github.com>
2019-11-27 23:05:50 +0000

    added a dropdown menu

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,7 @@ import {connect } from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
 import './header.styles.scss'
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 
 const Header =({ currentUser })=>(
@@ -31,6 +32,7 @@ const Header =({ currentUser })=>(
     )}
     <CartIcon/>
     </div>
+    <CartDropdown/>
     </div>
 );
 
